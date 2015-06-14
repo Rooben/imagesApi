da6e2471a1bf2cd5083918f5de7fc5c11b9a3179
Roland Oben <rooben@rogers.com>
2015-06-14 14:52:53 -0400

    I decided to create a custom modal that would take my images grid display to the next level. These files were directly affected by that.

--- app/scripts/controllers/grid.js
+++ app/scripts/controllers/grid.js
@@ -7,7 +7,7 @@
  * # GridCtrl
  * Controller of the ibmApp
  */
-angular.module('gridDisplay', ['ngResource'])
+angular.module('gridDisplay', ['ngResource', 'ui.bootstrap'])
   .config(function($stateProvider) {
     $stateProvider
       // GRID STATE  ===============================
@@ -28,47 +28,6 @@ angular.module('gridDisplay', ['ngResource'])
     }).error(function(error){
       console.log(error);
     });
-  }])
 
+  }]);
 
-  .directive("pinDisplay", function(){
-    return {
-      replace: true,
-      scope: {
-        img: '=pinDisplay'
-      },
-      templateUrl: '/views/grid_imageTemplate.html',
-      link: function(scope, elmt, attr){
-        function scalePinIcon(pId, duration, value){                // Animates the image icon to grow or shrink, depending on value.
-          TweenMax.to('#' + pId, duration, value);
-        }
-
-        scope.hovered = function(idx){
-          idx.mausOut = true;
-          scalePinIcon(idx.id, .15,  {                            //TweenMax animation scale image.
-            position: 'absolute',
-            width: "320px",
-            top: '-10px',
-            left: '-10px',
-            height: "220px",
-            zIndex: 900,
-            ease: Linear.easeNone
-          });
-        };
-
-        scope.unhovered = function(idx){
-          position: 'relative',
-          idx.mausOut = false;
-          scalePinIcon(idx.id, .15,  {           //TweenMax animation return to original state
-            width: "300px",
-            top: '0px',
-            left: '0px',
-            height: "200px",
-            zIndex: 10,
-            ease: Linear.easeNone
-          });
-        };
-      }
-
-    };
-  });
